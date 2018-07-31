eef660e05385eb89259f4e5c7988282dee03df8a
Mathieu Geli <m.geli@erpscan.com>
2018-07-31 14:24:32 +0300

    Full rewrite of this script

--- zoomeye.py
+++ zoomeye.py
@@ -1,119 +1,147 @@
 #!/usr/bin/env python
-import sys
-import json
-import requests
+#
+# Simple ZoomEye CLI
+# -- gelim
+
+from ansicolor import green,red,yellow,cyan
+from pprint import pprint
 import argparse
+import requests
+import urllib3
+import logging
+import pickle
+import json
+import os
 
-######## CHANGE THESE  (Or use `--email` and `--password` arguments) #########
-USER_EMAIL = "xxx"
-USER_PASSWORD = "xxx"
-##############################################################################
+urllib3.disable_warnings()
 
-parser = argparse.ArgumentParser(
-    description='Simple ZoomEye searcher, outputs IPs to stdout or file')
+headers = {"Content-Type": "application/json"}
+api={"login": "https://api.zoomeye.org/user/login",
+     "search": "https://api.zoomeye.org/host/search",
+     "info": "https://api.zoomeye.org/resources-info",
+}
+creds = {}
 
-parser.add_argument("search", help="Your ZoomEye Search")
-parser.add_argument(
-    "-p", "--pages", help="Number of pages to search (Default: 1)", type=int, default=5)
-parser.add_argument("--email", help="Your ZoomEye email", default=USER_EMAIL)
-parser.add_argument(
-    "--password", help="Your ZoomEye password", default=USER_PASSWORD)
-parser.add_argument(
-    "-s", "--save", help="Save output to results.txt", action="store_true")
-parser.add_argument("-pl", "--platform",
-                    help="Platforms to search, accepts \"host\" and \"web\" (Default: host)", default="host")
-parser.add_argument("--port", help="Include the port number in the results (e.g., 127.0.0.1:1337) (Only for host platform)", action="store_true")
-parser.add_argument("--short", help="Shows only the IP as results", action="store_true")
-parser.add_argument("--domain", help="Output the site address rather than the IP. (Only for web platform)", action="store_true")
-args = parser.parse_args()
+zoom_facets= ['country', 'os', 'product', 'service', 'port', 'device']
 
-QUERY = args.search
-PAGECOUNT = args.pages
-EMAIL = args.email
-PASSWORD = args.password
-
-SEARCH_TYPE = args.platform
-
-
-API_URL = "https://api.zoomeye.org"  # In case the ZoomEye API URL ever changes
-
-USER_DATA = '{"username": ' + '"' + EMAIL + '"' + \
-    ', "password":  ' + '"' + PASSWORD + '"' + '}'
-AUTH_REQUEST = requests.post(API_URL + '/user/login', data=USER_DATA)
-
-# Attempts to access the raw token, if there is a KeyError it means the credentials are incorrect
-
-
-def getToken():
-    try:
-        ACCESS_TOKEN = AUTH_REQUEST.json()['access_token']
-        return ACCESS_TOKEN
-    except KeyError:
-        print("[ERROR] Invalid Credentials, please specify an email and password either in this file or with `--email` and `--password` arguments")
-        quit()
-
-def detectSaveMode():
-    if args.save:
-        print("You have enabled save. All IPs will be saved to results.txt")
+def zoom_get_config(args):
+    conf_file = "%s/.zoomeye.p" % os.environ.get('HOME')
+    api = dict()
+    # command-line API key get precedence over other methods
+    if args.user and args.password:
+        creds['user'] = args.user
+        creds['password'] = args.password
+        pickle.dump(creds, open(conf_file, "wb")) # save to file
+        return creds
+    # if conf file exists, load it
+    if os.path.isfile(conf_file):
         try:
-            global resultsFile
-            resultsFile = open('results.txt', 'w')
+            creds = pickle.load(open(conf_file, "r"))
         except:
-            print(
-                "[ERROR] Could not write to results.txt, please check your permissions")
-            quit()
+            logging.error("Pickle file corrupted.")
+            exit(-1)
+        if not creds.get('user') or not creds.get('password'):
+            logging.error("Pickle file structure mismatch.")
+            exit(-2)
+        return creds
 
-# Loopy loop
-def getResult():
-    # This is the prefixed Token
-    TOKEN = "JWT " + getToken()
-    # Add the prefixed token to the headers
-    HEADERS = {"Authorization": TOKEN}
+def zoom_get_token(creds):
+    data = {"username": creds['user'], "password": creds['password']}
+    res = requests.post(api["login"], data=json.dumps(data),
+                        headers=headers)
+    if res.status_code != 200:
+        logging.error("HTTP request error (%d)" % res.status_code)
+        exit(-1)
+    return json.loads(res.text)["access_token"]
 
-    currentPage = 1
-    while currentPage <= PAGECOUNT:
-        if args.save:
-            print("\nPage " + str(currentPage))
+def zoom_search(token, search, page, facets):
+    params = {"query": search, "page": page, "facets": facets}
+    headers["Authorization"] = "JWT %s" % token
+    res = requests.get(api["search"], params=params, headers=headers, )
+    if res.status_code != 200:
+        logging.error("HTTP request error (%d)" % res.status_code)
+        exit(-2)
+    if facets:
+        return json.loads(res.text)["facets"]
+    else: return json.loads(res.text)
 
-        SEARCH = requests.get(API_URL + '/' + SEARCH_TYPE + '/search',
-                              headers=HEADERS, params={"query": QUERY, "page": currentPage})
-        response = json.loads(SEARCH.text)
-        if not response: quit() # no more results?
+def zoom_info(token):
+    headers["Authorization"] = "JWT %s" % token
+    res = requests.get(api["info"], headers=headers)
+    return json.loads(res.text)
+    
+def zoom_print_facets(result):
+    facets_k = result.keys()
+    count = str(result['device'][0]['count'])
+    facets_k.remove("device")
+    print "Total: %s" % red(count, bold=True)
+    for k in facets_k:
+        print "- %s" % green(k, bold=True)
+        for e in result[k]:
+            count = str(e['count'])
+            name = e['name']
+            if isinstance(name, int): name = str(name)
+            print count.ljust(8) + cyan(name).ljust(20)
+        print
+    return
 
-        for m in response["matches"]:
-            if SEARCH_TYPE == "host":
-                if args.port:
-                    resultItem = m["ip"] + ":" + str(m["portinfo"]["port"])
-                else:
-                    resultItem = m["ip"]
-                if not args.short:
-                    resultItem += "\tAS:%s (%s)\tLoc:%s /%s" % (m['geoinfo']['asn'], m['geoinfo']['aso'],
-                                                            m['geoinfo']['country']['code'],
-                                                            m['geoinfo']['country']['names']['en'])
+#
+# TODO: here we could diplay more info as additional columns for each IP
+#
+def print_results(search):
+    if args.port: print '\n'.join(["%s:%s" % (s['ip'], s['portinfo']['port']) for s in search])
+    else: print '\n'.join([s['ip'] for s in search])
 
-                if args.save:
-                    resultsFile.write(resultItem + "\n")
-                print(resultItem)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Simple ZoomEye CLI')
+    parser.add_argument("search", help="Your ZoomEye Search")
+    parser.add_argument("--user", help="ZoomEye API user", default=None)
+    parser.add_argument("--password", help="ZoomEye API password", default=None)
+    parser.add_argument("-l", "--limit", help="Limit number of results printed (default: 20)", type=int, default=20)
+    parser.add_argument("-f", "--facets", help="Facets to show (country,os,product,service,port,device)", default=None)
+    parser.add_argument("-i", "--info", help="Show account info", action="store_true")
+    parser.add_argument("--port", help="Show port with IP (default: False)", action="store_true")
+    parser.add_argument("--short", help="Shows only the IP as results", action="store_true")
+    parser.add_argument("--count", help="Only display number of results (default: False)", action="store_true")
+    args = parser.parse_args()
+    if args.facets == 'list':
+        print ','.join(zoom_facets)
+        exit(0)
+    logging.basicConfig(level=logging.INFO) # logging.DEBUG
 
-            if SEARCH_TYPE == "web":
-                if args.domain:
-                    resultItem = m["site"]
-                else:
-                    resultItem = m["ip"][0]
-                if args.save:
-                    resultsFile.write(resultItem + "\n")
-                print(resultItem)
-            ipCount()
-        currentPage += 1
+    creds = zoom_get_config(args)
+    token = zoom_get_token(creds)
+    logging.debug("Token: %s" % token)
+    if args.info:
+        pprint(zoom_info(token))
+        exit(0)
+    if args.facets: facets=args.facets + ",device"
+    else: facets=args.facets
+    
+    current_page = 1
 
+    search = zoom_search(token, args.search, current_page, facets)
+    current_page += 1
+    if facets:
+        zoom_print_facets(search)
+        exit(0)
+    else: print "Number of results: %s" % search["total"]
+    if args.count: exit(0)
+    if args.limit < 20: matches = search["matches"][:args.limit]
+    else: matches = search["matches"]
+    print_results(matches)
 
-def ipCount():
-    if args.save:
-        resultsFile.close()  # Close as write
-        ip_count = sum(1 for line in open("results.txt", "r"))  # Open as read
-        print("\n" + str(ip_count) + " IPs saved to results.txt")
+    # we asked for more pages?
+    pages_asked = int(args.limit / 20) + 1
 
-
-detectSaveMode()
-getResult()
-ipCount()
+    while current_page <= pages_asked:
+        res = zoom_search(token, args.search, current_page, facets)
+        matches = res["matches"]
+        if facets:
+            zoom_print_facets(search)
+            exit(0)
+        reminder = (args.limit - (current_page-1)*20)
+        if 0 < reminder < 20: print_results(matches[:reminder])
+        else: print_results(matches)
+        current_page += 1
+        
